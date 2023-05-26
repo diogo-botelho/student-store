8a6b8110f53985be219f57a946a5aa64362d7335
Diogo <diogofilipebotelho@gmail.com>
2023-05-26 16:41:35 -0400

    Added ProductDetail component

---
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import ProductView from "../ProductView/ProductView";
+
+const BACKEND_URL = "http://localhost:3001/store";
+
+/** ProductView component
+ *
+ * Takes the following props:
+ * handleAddItemToCart - handler function defined in the App.jsx component.
+ * handleRemoveItemToCart - handler function defined in the App.jsx component.
+ *
+ * App -> ProductDetail -> ProductView
+ */
+export default function ProductDetail({
+  handleAddItemToCart,
+  handleRemoveItemToCart,
+}) {
+  let { productId } = useParams();
+  const [product, setProduct] = useState(undefined);
+  const [needsProduct, setNeedsProduct] = useState(true);
+
+  useEffect(function loadProductInfo() {
+    async function getCurrentProduct(productId) {
+      try {
+        const currentProduct = (await axios.get(`${BACKEND_URL}/${productId}`))
+          .data;
+        setProduct(currentProduct);
+        setNeedsProduct(false);
+      } catch {
+        setProduct(undefined);
+      }
+    }
+    getCurrentProduct(productId);
+  }, []);
+
+  console.log(product, productId, `${BACKEND_URL}/${id}`);
+
+  return (
+    <div className="product-detail">
+      {needsProduct ? (
+        "Loading..."
+      ) : product ? (
+        <ProductView
+          product={product}
+          productId={productId}
+          quantity={product.quantity}
+          handleAddItemToCart={handleAddItemToCart}
+          handleRemoveItemToCart={handleRemoveItemToCart}
+        />
+      ) : (
+        <NotFound />
+      )}
+    </div>
+  );
+}
