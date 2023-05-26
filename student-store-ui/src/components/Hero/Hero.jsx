df2b3c3d59c94e7ee221a50cf095958fedb1ba03
Diogo <diogofilipebotelho@gmail.com>
2023-05-26 16:56:33 -0400

    Added Hero component

---
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export default function Hero() {
+  return (
+    <div className="hero">
+        <div className="intro">Welcome!</div>
+      <img className="hero-img" src="" />
+    </div>
+  )
+}
