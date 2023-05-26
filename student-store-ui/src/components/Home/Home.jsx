b0102155cdf8f5b5571b1155f0e2ab236c249616
Diogo <diogofilipebotelho@gmail.com>
2023-05-26 16:56:53 -0400

    Added Home component

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,10 +1,19 @@
-import * as React from "react"
-import "./Home.css"
+import * as React from "react";
+import "./Home.css";
 
-export default function Home() {
+export default function Home({
+  products,
+  handleAddItemToCart,
+  handleRemoveItemToCart,
+}) {
   return (
     <div className="home">
-      <p>Home</p>
+      <Hero />
+      <ProductGrid
+        products={products}
+        handleAddItemToCart={handleAddItemToCart}
+        handleRemoteItemToCart={handleRemoveItemToCart}
+      />
     </div>
-  )
+  );
 }
