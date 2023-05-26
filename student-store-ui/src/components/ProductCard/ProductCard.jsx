2c9c43d21d36035651e88ec8bd1ec7f4ffb3861d
Diogo <diogofilipebotelho@gmail.com>
2023-05-26 16:10:12 -0400

    Added ProductCard component

---
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+/**
+ *
+ * @param
+ * productCardDetails, an object with the following properties:
+ * product - product object.
+ * productId - number representing the id of the product.
+ * quantity - quantity for this product found in the shoppingCart.
+ * handleAddItemToCart - handler function defined in the App.jsx component.
+ * handleRemoveItemToCart - handler function defined in the App.jsx component.
+ * showDescription - boolean value that determines whether to show description.
+ * @returns
+ */
+export default function ProductCard({ productCardDetails }) {
+  const {
+    product,
+    productId,
+    quantity,
+    handleAddItemToCart,
+    handleRemoteItemToCart,
+    showDescription,
+  } = productCardDetails;
+
+  return (
+    <div className="product-card">
+      <div className="product-name">Name: {product.name}</div>
+      <div className="product-price">Price: {product.price}</div>
+      {showDescription && (
+        <div className="product-description">
+          Description: {product.description}
+        </div>
+      )}
+      <div className="media">
+        <Link to={`/products/${productId}`}>
+          <img src={product.image}></img>
+        </Link>
+      </div>
+      <button className="add" onClick={handleAddItemToCart}></button>
+      <button className="remove" onClick={handleRemoteItemToCart}></button>
+    {quantity> 0 && <div className="product-quantity">Items in car: {quantity}</div>
+    </div>
+  );
+}
